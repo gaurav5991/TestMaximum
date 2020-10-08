13e92d77c198854818fbfd79898023b2c18e9969
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-10-08 21:50:46 +0530

    [Gaurav]ADDED: Find Maximum Integer From Given Integers

---
+++ src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.testmaximum;
+
+public class TestMaximum {
+    /*Welcome Message*/
+    public boolean printWelcomeMessage() {
+        System.out.println("Welcome to Test maximum Number Program");
+        return true;
+    }
+    /*Find Maximum Integer from given Integers*/
+    public int findMaximumNumber(Integer firstNumber,Integer secondNumber, Integer thirdNumber){
+        Integer maximum = firstNumber;
+        if(secondNumber.compareTo(maximum)>0)
+            maximum = secondNumber;
+        if(thirdNumber.compareTo(maximum)>0)
+            maximum = thirdNumber;
+        return maximum;
+    }
+}
--- src/test/java/com/bridgelabz/testmaximum/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/testmaximum/TestMaximumTest.java
@@ -2,12 +2,36 @@ package com.bridgelabz.testmaximum;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestMaximumTest {
+    TestMaximum testMaximum = new TestMaximum();
+
     @BeforeClass
     public static void beforeClass() {
         TestMaximum testMaximum = new TestMaximum();
         boolean welcomeMessage = testMaximum.printWelcomeMessage();
         Assert.assertTrue(welcomeMessage);
+    }
+    /*Test Cases for Integer Numbers*/
+    @Test
+    public void givenThreeIntegers_ifMaxIsFirstInteger_shouldReturnFirstNumber() {
+        int maximumNumber = testMaximum.findMaximumNumber(60, 30, 30);
+        System.out.println("The Maximum Number: " + maximumNumber);
+        Assert.assertEquals(60, maximumNumber);
+    }
+
+    @Test
+    public void givenThreeIntegers_ifMaxIsSecondInteger_shouldReturnSecondNumber() {
+        int maximumNumber = testMaximum.findMaximumNumber(30, 80, 30);
+        System.out.println("The Maximum Number: " + maximumNumber);
+        Assert.assertEquals(80, maximumNumber);
+    }
+
+    @Test
+    public void givenThreeIntegers_ifMaxIsThirdInteger_shouldReturnThirdNumber() {
+        int maximumNumber = testMaximum.findMaximumNumber(30, 10, 70);
+        System.out.println("The Maximum Number: " + maximumNumber);
+        Assert.assertEquals(70, maximumNumber);
     }
 }
